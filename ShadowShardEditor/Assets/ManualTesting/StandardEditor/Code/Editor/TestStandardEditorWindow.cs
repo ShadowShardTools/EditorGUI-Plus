cf39c24cc3c641a580b1c1ddbe641da785f48aec
Maxim Moskalyk <ictic.step@gmail.com>
2024-06-16 16:50:28 +0300

    Testing exceptions fixed.

--- ShadowShardEditor/Assets/ManualTesting/StandardEditor/Code/Editor/TestStandardEditorWindow.cs
+++ ShadowShardEditor/Assets/ManualTesting/StandardEditor/Code/Editor/TestStandardEditorWindow.cs
@@ -10,6 +10,7 @@ namespace ManualTesting.StandardEditor.Code.Editor
     {
         private ShadowShardData _shadowShardData;
         private ShadowShardEditor _shadowShardEditor;
+        private SerializedObject _serializedShadowShardData;
 
         [MenuItem("Window/EditorTesting/Standard Editor Tests")]
         public static void ShowWindow() =>
@@ -19,11 +20,20 @@ namespace ManualTesting.StandardEditor.Code.Editor
         {
             _shadowShardData = EditorGUILayout.ObjectField("Test data", _shadowShardData, typeof(ShadowShardData), false) as ShadowShardData;
             _shadowShardEditor ??= new ShadowShardEditor();
-            if (_shadowShardData == null)
+            if(_shadowShardData == null)
             {
                 EditorGUILayout.HelpBox("Test data is not set!", MessageType.Error);
                 return;
             }
+
+            if(_serializedShadowShardData == null)
+            {
+                _serializedShadowShardData = _shadowShardData.GetSerializedObject();
+                _serializedShadowShardData.ApplyModifiedProperties();
+                _serializedShadowShardData.Update();
+            }
+
+            EditorGUI.BeginChangeCheck();
 
             _shadowShardEditor.DrawVertical(_shadowShardData.Styles, () => { });
             _shadowShardEditor.DrawIndented(_shadowShardData.IndentLevel, () => { });
@@ -33,13 +43,35 @@ namespace ManualTesting.StandardEditor.Code.Editor
             _shadowShardEditor.DrawGroup(() => { });
             _shadowShardEditor.DrawGroup(_shadowShardData.Label, _shadowShardData.IsDisabled, () => { });
             _shadowShardEditor.DrawGroup(_shadowShardData.Label, () => { });
-            _shadowShardData.SliderValue = _shadowShardEditor.DrawSlider(new GUIContent("Slider"), null, new FloatRange(0, 1), _shadowShardData.IndentLevel);
-            _shadowShardData.IntSliderValue = _shadowShardEditor.DrawIntSlider(new GUIContent("Int Slider"), null, new IntRange(0, 10), _shadowShardData.IndentLevel);
-            _shadowShardEditor.DrawFromVector3ParamSlider(new GUIContent("Vector3 Slider"), null, Vector3Param.X, new FloatRange(0, 1), _shadowShardData.IndentLevel);
-            _shadowShardEditor.DrawVector3Sliders(new GUIContent("X"), new GUIContent("Y"), new GUIContent("Z"), null, new FloatRange(0, 1), _shadowShardData.IndentLevel);
-            _shadowShardData.FloatRangeValue = _shadowShardEditor.DrawMinMaxSlider(new GUIContent("Min Max Slider"), null, null, new FloatRange(0, 1), _shadowShardData.IndentLevel);
-            _shadowShardData.FloatRangeValue = _shadowShardEditor.DrawMinMaxVector4StartSlider(new GUIContent("Vector4 Start Slider"), null, new FloatRange(0, 1), _shadowShardData.IndentLevel);
-            _shadowShardData.FloatRangeValue = _shadowShardEditor.DrawMinMaxVector4EndSlider(new GUIContent("Vector4 End Slider"), null, new FloatRange(0, 1), _shadowShardData.IndentLevel);
+
+            _shadowShardData.SliderValue = _shadowShardEditor.DrawSlider(
+                new GUIContent("Slider"), _serializedShadowShardData.FindProperty("SliderValue"), new FloatRange(0, 1), _shadowShardData.IndentLevel);
+
+            _shadowShardData.IntSliderValue = _shadowShardEditor.DrawIntSlider(
+                new GUIContent("Int Slider"), _serializedShadowShardData.FindProperty("IntSliderValue"), new IntRange(0, 10), _shadowShardData.IndentLevel);
+
+            _shadowShardEditor.DrawFromVector3ParamSlider(
+                new GUIContent("Vector3 Slider"), _serializedShadowShardData.FindProperty("Vector3Value"), Vector3Param.X, new FloatRange(0, 1), _shadowShardData.IndentLevel);
+
+            _shadowShardEditor.DrawVector3Sliders(
+                new GUIContent("X"), new GUIContent("Y"), new GUIContent("Z"), _serializedShadowShardData.FindProperty("Vector3Value"), new FloatRange(0, 1),
+                _shadowShardData.IndentLevel);
+
+            _shadowShardEditor.DrawMinMaxSlider(
+                new GUIContent("Min Max Slider"), _serializedShadowShardData.FindProperty("MinValue"), _serializedShadowShardData.FindProperty("MaxValue"),
+                new FloatRange(0, 1), _shadowShardData.IndentLevel);
+
+            _shadowShardEditor.DrawMinMaxVector4StartSlider(
+                new GUIContent("Vector4 Start Slider"), _serializedShadowShardData.FindProperty("Vector4Value"), new FloatRange(0, 1), _shadowShardData.IndentLevel);
+
+            _shadowShardEditor.DrawMinMaxVector4EndSlider(
+                new GUIContent("Vector4 End Slider"), _serializedShadowShardData.FindProperty("Vector4Value"), new FloatRange(0, 1), _shadowShardData.IndentLevel);
+
+            if(EditorGUI.EndChangeCheck())
+            {
+                _serializedShadowShardData.ApplyModifiedProperties();
+                _serializedShadowShardData.Update();
+            }
         }
     }
 }
--- ShadowShardEditor/Assets/ManualTesting/StandardEditor/Code/TestSciptableObject.cs
+++ ShadowShardEditor/Assets/ManualTesting/StandardEditor/Code/TestSciptableObject.cs
@@ -1,4 +1,5 @@
 using ShadowShard.Editor.Range;
+using UnityEditor;
 using UnityEngine;
 
 namespace ManualTesting.StandardEditor.Code
@@ -11,9 +12,14 @@ namespace ManualTesting.StandardEditor.Code
         public bool IsDisabled = false;
         public GUIContent Label = new GUIContent("Test Label");
         public float SliderValue = 0.5f;
+        public float MinValue = 0.0f;
+        public float MaxValue = 1.0f;
         public int IntSliderValue = 5;
         public Vector3 Vector3Value = new Vector3(1, 2, 3);
         public Vector4 Vector4Value = new Vector4(1, 2, 3, 4);
         public FloatRange FloatRangeValue = new FloatRange(0, 1);
+        
+        public SerializedObject GetSerializedObject() =>
+            new(this);
     }
 }
