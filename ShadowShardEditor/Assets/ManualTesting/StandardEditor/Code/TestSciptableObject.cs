fe0b3e04a3a355894ee67c301c792c04d510e0c5
Maxim Moskalyk <ictic.step@gmail.com>
2024-06-16 15:55:46 +0300

    Assebly definitions created; new test data added.

--- ShadowShardEditor/Assets/ManualTesting/StandardEditor/Code/TestSciptableObject.cs
+++ ShadowShardEditor/Assets/ManualTesting/StandardEditor/Code/TestSciptableObject.cs
@@ -1,3 +1,4 @@
+using ShadowShard.Editor.Range;
 using UnityEngine;
 
 namespace ManualTesting.StandardEditor.Code
@@ -5,9 +6,14 @@ namespace ManualTesting.StandardEditor.Code
     [CreateAssetMenu(fileName = "TestData", menuName = "Static data/Test/Standard editor")]
     public class ShadowShardData : ScriptableObject
     {
-        public GUIStyle Styles;
-        public int IndentLevel;
-        public bool IsDisabled;
-        public GUIContent Label;
+        public GUIStyle Styles = new GUIStyle();
+        public int IndentLevel = 2;
+        public bool IsDisabled = false;
+        public GUIContent Label = new GUIContent("Test Label");
+        public float SliderValue = 0.5f;
+        public int IntSliderValue = 5;
+        public Vector3 Vector3Value = new Vector3(1, 2, 3);
+        public Vector4 Vector4Value = new Vector4(1, 2, 3, 4);
+        public FloatRange FloatRangeValue = new FloatRange(0, 1);
     }
 }
