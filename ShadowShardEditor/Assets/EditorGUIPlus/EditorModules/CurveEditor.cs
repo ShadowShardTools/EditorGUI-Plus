472c86faf16873aea9e6e5422cd8e764454f7219
narberalAntares <valerii.deineka.volodymyrovych@gmail.com>
2024-06-22 15:19:59 +0300

    Added Curve property method to CurveEditor

--- ShadowShardEditor/Assets/EditorGUIPlus/EditorModules/CurveEditor.cs
+++ ShadowShardEditor/Assets/EditorGUIPlus/EditorModules/CurveEditor.cs
@@ -13,6 +13,28 @@ namespace EditorGUIPlus.EditorModules
             _groupEditor = groupEditor;
         }
         
+        internal AnimationCurve DrawAnimationCurve(GUIContent label, ref AnimationCurve curve, int indentLevel = 0, 
+            Action onChangedCallback = null)
+        {
+            AnimationCurve tempCurve = curve;
+            _groupEditor.DrawIndented(indentLevel, Draw);
+            curve = tempCurve;
+            
+            return curve;
+
+            void Draw()
+            {
+                EditorGUI.BeginChangeCheck();
+                AnimationCurve newValue = EditorGUILayout.CurveField(label, tempCurve);
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    tempCurve = newValue;
+                    onChangedCallback?.Invoke();
+                }
+            }
+        }
+        
         internal AnimationCurve DrawAnimationCurve(GUIContent label, SerializedProperty property, int indentLevel = 0, 
             Action onChangedCallback = null)
         {
