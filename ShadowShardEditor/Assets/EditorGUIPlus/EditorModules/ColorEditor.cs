4be7efb826a2e2215edd6e6fe1e1594fdb5f44a3
narberalAntares <valerii.deineka.volodymyrovych@gmail.com>
2024-06-22 15:17:28 +0300

    Added Color property method to ColorEditor

--- ShadowShardEditor/Assets/EditorGUIPlus/EditorModules/ColorEditor.cs
+++ ShadowShardEditor/Assets/EditorGUIPlus/EditorModules/ColorEditor.cs
@@ -15,6 +15,50 @@ namespace EditorGUIPlus.EditorModules
             _groupEditor = groupEditor;
         }
         
+        internal Color DrawColor(GUIContent label, ref Color color, bool showAlpha = true,
+            bool hdr = false, int indentLevel = 0, Action onChangedCallback = null)
+        {
+            Color tempColor = color;
+            _groupEditor.DrawIndented(indentLevel, Draw);
+            color = tempColor;
+    
+            return color;
+
+            void Draw()
+            {
+                EditorGUI.BeginChangeCheck();
+                Color newValue = EditorGUILayout.ColorField(label, tempColor, true, showAlpha, hdr);
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    tempColor = newValue;
+                    onChangedCallback?.Invoke();
+                }
+            }
+        }
+        
+        internal Gradient DrawGradient(GUIContent label, ref Gradient gradient, bool hdr = false, 
+            int indentLevel = 0, Action onChangedCallback = null)
+        {
+            Gradient tempGradient = gradient;
+            _groupEditor.DrawIndented(indentLevel, Draw);
+            gradient = tempGradient;
+            
+            return gradient;
+
+            void Draw()
+            {
+                EditorGUI.BeginChangeCheck();
+                Gradient newValue = EditorGUILayout.GradientField(label, tempGradient, hdr);
+                
+                if (EditorGUI.EndChangeCheck())
+                {
+                    tempGradient = newValue;
+                    onChangedCallback?.Invoke();
+                }
+            }
+        }
+        
         internal Color DrawColor<TProperty>(GUIContent label, TProperty property, bool showAlpha = true,
             bool hdr = false, int indentLevel = 0, Action onChangedCallback = null)
         {
